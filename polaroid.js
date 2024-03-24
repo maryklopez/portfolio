80280cf360a3efb3a0e1c984ecf3479b5de6d8e9
marylopez <mary.lopez@mail.utoronto.ca>
2024-03-24 00:41:01 -0400

    m

--- polaroid.js
+++ polaroid.js
@@ -1,3 +1,7 @@
+
+script.src = 'https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js';
+
+
 (function() {
 	$.fn.card_slideshow = function(args) {
 		this.each(function(index, slideshow_el) {
